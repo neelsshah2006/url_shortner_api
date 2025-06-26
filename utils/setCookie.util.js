574ee853a6ad054e324d547f4a5c8fa082662e36
neelsshah2006 <neelsshah2006@gmail.com>
2025-06-26 17:15:42 +0530

    Update Cookie Setting

--- utils/setCookie.util.js
+++ utils/setCookie.util.js
@@ -1,8 +1,10 @@
+const isProd = process.env.NODE_ENV === "production";
+
 const setCookie = (res, tokenName, token) => {
   return res.cookie(tokenName, token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    secure: isProd,
+    sameSite: isProd ? "none" : "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
